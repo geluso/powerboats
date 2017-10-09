b723dd3a1ee3b1e6281ae71cf0f1cdbcfb1f119c
Steve Geluso <stevegeluso@gmail.com>
2017-10-09 16:07:49 -0700

    syntastic

--- js/geo/point.js
+++ js/geo/point.js
@@ -12,7 +12,7 @@ Point.distance = function(x1, y1, x2, y2) {
   var yy = Math.pow(y1 - y2, 2);
   var distance = Math.sqrt(xx + yy);
   return distance;
-}
+};
 
 Point.draw = function(ctx, x, y, radius) {
   ctx.save();
@@ -31,4 +31,4 @@ Point.draw = function(ctx, x, y, radius) {
   ctx.stroke(path);
 
   ctx.restore();
-}
+};
