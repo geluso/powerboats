9345280696dcb518a5b69bb197a779d3184dfebd
Steve Geluso <stevegeluso@gmail.com>
2017-10-09 19:15:58 -0700

    disabling showing single coords

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var CONFIG = {
   DRAW_COORDS: false,
-  DRAW_HOVER_COORDS: true,
+  DRAW_HOVER_COORDS: false,
   MIN_DICE: 1,
   MAX_DICE: 3,
   COLORS: ["yellow", "purple", "white", "green", "orange"],
